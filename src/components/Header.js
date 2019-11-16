d64761e171c3b5a2b6ee9aa51f148673ec9f6dba
AreTea <tbusinessr@gmail.com>
2019-11-16 17:28:41 -0500

    basic header and nav component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav.js';
+
+function Header() {
+  return (
+    <div>
+        Welcome to the Time Blocker!
+        <Nav />
+    </div>
+  );
+}
+
+export default Header;
